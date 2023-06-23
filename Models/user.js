1b512da5fb599d5220e0a7129e7088ece4b22586
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-23 19:43:30 -0300

    registrando novo usuario estamos usando postman aula17 dnv lmao

--- Models/user.js
+++ Models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const crypto = require('crypto')
-const uidv1 = require('uuid/v1')
+const {v1: uuidv1} = require('uuid')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -43,15 +43,21 @@ userSchema.virtual('password')
     this.salt = uuidv1()
     this.hashed_password = this.encryptPassword(password)
   })
-  .get(function(){
+  .get(function () {
     return this._password
   })
 
 userSchema.methods = {
-  encryptPassword: function(password){
-    if(!password) return '';
-    try{
-      return crypto.createHmac('sha1',this.salt)
+  encryptPassword: function (password) {
+    if (!password) return '';
+    try {
+      return crypto.createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    }catch(err){
+      return ""
     }
   }
 }
+
+module.exports = mongoose.model("User", userSchema)
