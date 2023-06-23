178cb2609dd70a8b7b35395974d54f8d930fdf89
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-23 09:14:19 -0300

    mensagem no console server is running on port x

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+// const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
 
 app.get('/', (req,res) =>{
-  res.send("hello from node")
+  res.send("hello from node updated")
 })
 
 const port = process.env.PORT || 8000
+
+app.listen(port, ()=>{
+  console.log(`Server is running on port ${port}`)
+})
