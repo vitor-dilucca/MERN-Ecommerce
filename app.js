8c8f4483dff756a339400a61e75f1c4e624a94a7
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-23 09:35:51 -0300

    conectando com o db

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
-// const mongoose = require('mongoose')
-const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-app.get('/', (req,res) =>{
+// app
+const app = express()
+
+// db
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true }
+)
+  .then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
+
+// routes
+app.get('/', (req, res) => {
   res.send("hello from node updated")
 })
 
 const port = process.env.PORT || 8000
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
