9d3e5ecbcc55977f5028138909816a2c019c7eaa
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-23 16:40:40 -0300

    criado pasta controllers com o controllers/user.js

--- routes/User.js
+++ routes/User.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-  res.send("hello from node asd")
-})
+const {sayHi} = require('../controllers/User')
+
+router.get('/', sayHi)
 
 module.exports = router
