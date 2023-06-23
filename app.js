37923b04b25eb8edae2861e616ce541d6adb9bfb
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-23 16:32:33 -0300

    passando a route para outro arquivo e usando o mesmo como middlewre no app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
+//import routes
+const userRoutes = require('./routes/User')
 
 // app
 const app = express()
@@ -8,18 +10,16 @@ const app = express()
 // db
 mongoose.connect(
   process.env.MONGO_URI,
-  { useNewUrlParser: true }
-)
-  .then(() => console.log('DB Connected'))
+  {
+    useNewUrlParser: true,
+  }).then(() => console.log('DB Connected'))
 
 mongoose.connection.on('error', err => {
   console.log(`DB connection error: ${err.message}`)
 });
 
-// routes
-app.get('/', (req, res) => {
-  res.send("hello from node updated")
-})
+//routes
+app.use('/api' ,userRoutes)
 
 const port = process.env.PORT || 8000
 
