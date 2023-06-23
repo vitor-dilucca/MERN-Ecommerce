635bd89c1c0623f3b8a99b4b00cec959f15c5eae
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-23 17:25:26 -0300

    virtual property in a Mongoose user schema. This virtual property is typically used for handling password-related functionality, such as encryption and validation.

--- Models/user.js
+++ Models/user.js
@@ -35,3 +35,23 @@ const userSchema = new mongoose.Schema({
 },
   { timestamps: true }
 )
+
+// virtual field
+userSchema.virtual('password')
+  .set(function (password) {
+    this._password = password
+    this.salt = uuidv1()
+    this.hashed_password = this.encryptPassword(password)
+  })
+  .get(function(){
+    return this._password
+  })
+
+userSchema.methods = {
+  encryptPassword: function(password){
+    if(!password) return '';
+    try{
+      return crypto.createHmac('sha1',this.salt)
+    }
+  }
+}
